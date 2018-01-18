2d7e92c4336902ca6ee24f9e332362b578387324
Alvaro De La Torre <alvarodltp@gmail.com>
2018-01-18 12:08:13 -0600

    Adds omniauth files for facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,21 +6,14 @@ class User < ApplicationRecord
 
   devise :omniauthable, :omniauth_providers => [:facebook]
 
-	def self.from_omniauth(auth)
-	 user = find_by(email: auth.info.email)
-    if user
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.save
-    end
-	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-	    user.email = auth.info.email
-	    user.password = Devise.friendly_token[0,20]
-	    #user.name = auth.info.name   # assuming the user model has a name
-	    #user.image = auth.info.image # assuming the user model has an image
-	    # If you are using confirmable and the provider(s) you use validate emails, 
-	    # uncomment the line below to skip the confirmation emails.
-	    # user.skip_confirmation!
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
   	end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,12 @@ Rails.application.routes.draw do
 
   get 'workout_controller/results'
 
+  get 'auth/:provider/callback' => 'sessions#create'
+
+  get 'auth/failure' => redirect('/')
+
+  get 'signout' => 'sessions#destroy', :as => "signout"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
