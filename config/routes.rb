790e40946283cbbc98b1a7b868c153926290cf2d
Alvaro <alvarodltp@gmail.com>
2018-02-02 13:47:42 -0600

    Removed Devise and Omniauth to reinstall from scratch

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => "registrations" }
 
   root 'workout#index'
 
@@ -11,12 +10,4 @@ Rails.application.routes.draw do
 
   get 'workout_controller/results'
 
-  get 'auth/:provider/callback' => 'sessions#create'
-
-  get 'auth/failure' => redirect('/')
-
-  get 'signout' => 'sessions#destroy', :as => "signout"
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
